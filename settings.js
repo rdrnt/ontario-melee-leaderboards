3c92b799125333ee839276a1ba9c32891bddbffb
riley drnt <rileyduraant@gmail.com>
2022-12-21 15:50:52 -0500

    [CHORE]: Change spreadsheet key

--- settings.js
+++ settings.js
@@ -13,5 +13,5 @@ module.exports = {
 
   // Google sheets ID for player connect codes.
   // https://docs.google.com/spreadsheets/d/[YOUR ID]
-  spreadsheetID: '1DPIFD0RUA3yjruregmFUbUJ7ccdOjVB2LBp0goHvL-A',
+  spreadsheetID: '1g_Z5IUw2mDdiy33WtnCLZDr9OQ3Xir2pfx-gjbYqnEg',
 };
